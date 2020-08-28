305c35a3664a8b1ce8a53b1d440ac6d63a89f9a9
PedroSousa2020 <sousapedro747@gmail.com>
2020-08-28 14:44:25 +0100

    Navegacao entre componente home e produtos

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomeComponent} from './views/home/home.component';
+import {ProductCrudComponent} from './views/product-crud/product-crud.component';
+import { from } from 'rxjs';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:"",
+    component: HomeComponent
+  },
+  {
+    path:"products",
+    component: ProductCrudComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
